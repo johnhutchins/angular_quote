1cbd8629f7edd6ed001b2cfac0252061c5c45b47
John Hutchins <johnwallshutchins@gmail.com>
2017-09-15 08:38:24 -0400

    init files

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+<div id="main">
+  <h1>{{title}}</h1>
+  <label>name: </label>
+  <input [(ngModel)]="hero.name" placeholder="name">
+  <div class="left-padding"><label>id: </label>{{hero.id}}</div>
+  <div class="left-padding"><label>name: </label>{{hero.name}}</div>
+</div>`, 
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import Appcomponent = require("./app.component");
+import Hero = Appcomponent.Hero;
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

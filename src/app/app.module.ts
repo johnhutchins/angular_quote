b5c7a71f1a9632b8ee85586a5a8d04b4d1b940f7
John Hutchins <johnwallshutchins@gmail.com>
2017-09-15 10:37:35 -0400

    add productComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { ProductComponent } from './app.productComponent';
 import { AppComponent }  from './app.component';
 import Appcomponent = require("./app.component");
 import Hero = Appcomponent.Hero;
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductComponent],
+  bootstrap: [AppComponent, ProductComponent]
 })
 export class AppModule { }
---
+++ src/app/app.productComponent.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from '@angular/core';
+
+export class Product {
+  product: number;
+  name: string;
+  price: number;
+}
+
+@Component({
+  selector: 'quote-gen',
+  template: `
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+  <div>
+    <table>
+      <tr><td>
+      {{description}}
+</td></tr>
+<tr><td>
+  {{price}}
+</td></tr>
+  </table>
+  </div>
+
+`
+})
+
+export class ProductComponent {
+  title = 'ASEBA Quote Generator';
+  description = 'Product:';
+  price = 'Price:';
+
+}
